92c5832a3acd33b0ad812fff4779ca21185bfaad
Michael Gharbi <gharbi@mit.edu>
2016-02-09 16:09:54 -0500

    updates run script

--- matlab/run.m
+++ matlab/run.m
@@ -1,3 +1,14 @@
+% -------------------------------------------------------------------
+% File:    run.m
+% Author:  Michael Gharbi <gharbi@mit.edu>
+% Created: 2016-02-02
+% -------------------------------------------------------------------
+% 
+% 
+% 
+% ------------------------------------------------------------------#
+
+
 addpath('../lib/mex');
 addpath('viz_warp');
 addpath('io');
@@ -5,12 +16,10 @@ addpath('io');
 A = load_video('../data/derailleur_01.mov');
 B = load_video('../data/derailleur_02.mov');
 
-A = A(1:30,1:30,1:30,:);
-B = B(1:30,1:30,1:30,:);
+% Warp
 warp = stwarp(A, B);
-size(warp)
 
+% Visualization
 [spatial,temporal] = viz_warp(warp);
-
 save_video(spatial,'../output/spatial.mp4');
 save_video(temporal,'../output/temporal.mp4');
